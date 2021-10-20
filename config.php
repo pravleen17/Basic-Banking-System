e0b0fb7122a0eb0a6481c53e433d91d39c30705f
pravleen17 <64193118+pravleen17@users.noreply.github.com>
2021-10-20 12:30:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d');
+	$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','pravleen','12345678@','malyaraaj-313537e38d');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
