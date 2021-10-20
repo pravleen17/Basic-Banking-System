59f36d6071fa97a4b66688d46629211c769766c0
pravleen17 <64193118+pravleen17@users.noreply.github.com>
2021-10-20 12:47:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </div>
       </div>
       <footer class="text-center mt-5 py-2">
-        <p>&copy 2020. Made by <b>SHIVANGI AGARWAL</b> <br> The Sparks Foundation</p>
+        <p>&copy 2020. Made by <b>PRAVLEEN KAUR</b> <br> The Sparks Foundation</p>
       </footer>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
